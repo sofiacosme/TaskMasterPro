ca84542410f860c962118fa8107f6727e0306ad2
Johan Camilo <johancamiloc10@gmail.com>
2025-09-19 10:25:15 -0500

    Se agrega la clase con la logica para el tutorial

---
+++ java/task/master/pro/home/TutorialFragment.java
@@ -0,0 +1,55 @@
+package task.master.pro.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import task.master.pro.R;
+
+public class TutorialFragment extends Fragment {
+
+    private YouTubePlayerView youTubePlayerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tutorial, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        youTubePlayerView = view.findViewById(R.id.youtubePlayerView);
+        getLifecycle().addObserver(youTubePlayerView);
+
+
+        String videoId = "ZokFvdw8LZs"; //
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                youTubePlayer.cueVideo(videoId, 0);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (youTubePlayerView != null) {
+            youTubePlayerView.release();
+        }
+    }
+}
